cacc2819906cff4659303dd10e24c9b2589ad36f
Colby Pate <cbpate08@gmail.com>
2022-08-09 20:48:03 -0500

    initial commit

---
+++ Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string courseName = "TrueCoders";
+int startDay = 5;
+char at = '@';
+bool inProgress = true;
+double priceOfDesk = 225.99;
+decimal priceOfChair = 115.99m;
+
+Console.WriteLine($"My first day {at} {courseName} was on August {startDay}th.");
+if (inProgress == true)
+{
+    Console.WriteLine($"Before staring the class I bought myself a desk for {priceOfDesk} and a chair for {priceOfChair}.");
+}
